792c0d2c5e1a1d03c4109235c8f2d491fba9d18e
Flavio Silva <fal_silva@hotmail.com>
2024-11-25 16:55:00 -0300

    criando a função de area

--- script.js
+++ script.js
@@ -4,10 +4,41 @@ document.querySelectorAll('.item').forEach(item => {
     item.addEventListener('dragend', dragEnd)
 })
 
-//Funções
+document.querySelectorAll('.area').forEach( area => {
+    area.addEventListener('dragover', dragOver)
+    area.addEventListener('dragleave', dragLeave)
+    area.addEventListener('drop', drop)
+
+})
+
+//Funções do item
 function dragStart(e){
     e.currentTarget.classList.add('dragging')
 }
 function dragEnd(e){
     e.currentTarget.classList.remove('dragging')
+}
+
+//Funções da area
+function dragOver(e){
+    if(e.currentTarget.querySelector('.item') === null){
+        e.preventDefault()
+        e.currentTarget.classList.add('hover')
+    }
+    //console.log('passou por cima!')
+}
+
+function dragLeave(e){
+    e.currentTarget.classList.remove('hover')
+    //console.log('saiu de uma area dropavel!')
+}
+
+function drop(e){
+    e.currentTarget.classList.remove('hover')
+
+    if(e.currentTarget.querySelector('.item') === null){
+        let dragItem = document.querySelector('.item.dragging')
+        e.currentTarget.appendChild(dragItem)
+    }
+    //console.log(e.currentTarget)
 }
